985ef0a95b88316bbce3e9d16c822398b787e39c
Maximiliano Principe <mlpeducacionit@gmail.com>
2025-09-30 16:29:05 -0300

    recorriendo el array y los objetos

--- clase-03/02-practica-integradora/src/main.js
+++ clase-03/02-practica-integradora/src/main.js
@@ -1,11 +1,21 @@
 import './style.css'
 
-const producto1 = { id: 1, nombre: 'Notebook', precio: 1200, img: 'https://placehold.co/400'}
-const producto2 = { id: 2, nombre: 'PC', precio: 900, img: 'https://placehold.co/400'}
-const producto3 = { id: 3, nombre: 'Auriculares', precio: 200, img: 'https://placehold.co/400'}
-const producto4 = { id: 4, nombre: 'Mouse', precio: 100, img: 'https://placehold.co/400'}
-const producto5 = { id: 5, nombre: 'Monitor', precio: 300, img: 'https://placehold.co/400'}
+const producto1 = { id: 1, nombre: 'Notebook', precio: 1200, img: 'https://placehold.co/400' } // objeto
+const producto2 = { id: 2, nombre: 'PC', precio: 900, img: 'https://placehold.co/400' } // objeto
+const producto3 = { id: 3, nombre: 'Auriculares', precio: 200, img: 'https://placehold.co/400'} // objeto
+const producto4 = { id: 4, nombre: 'Mouse', precio: 100, img: 'https://placehold.co/400' }
+const producto5 = { id: 5, nombre: 'Monitor', precio: 300, img: 'https://placehold.co/400' }
 
-const productos = [producto1, producto2, producto3, producto4, producto5]
+// array de objetos (productos)
+const productos = [producto1, producto2, producto3, producto4, producto5] // array
 
 console.log(productos)
+
+// Recorro el array de objetos y voy mostrar objeto por objeto
+for (const producto of productos) {
+    console.log(producto);
+    // De cada producto, extraigo cada clave
+    for (const clave in producto) {       
+        console.log(clave, ' : ', producto[clave]);
+    }
+}
