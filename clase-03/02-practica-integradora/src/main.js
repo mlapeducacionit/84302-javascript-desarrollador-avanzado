bab0645a338f7c411c99c244d7a467e0507ada8c
Maximiliano Principe <mlpeducacionit@gmail.com>
2025-09-30 16:24:33 -0300

    empezando con el array

--- clase-03/02-practica-integradora/src/main.js
+++ clase-03/02-practica-integradora/src/main.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+const producto1 = { id: 1, nombre: 'Notebook', precio: 1200, img: 'https://placehold.co/400'}
+const producto2 = { id: 2, nombre: 'PC', precio: 900, img: 'https://placehold.co/400'}
+const producto3 = { id: 3, nombre: 'Auriculares', precio: 200, img: 'https://placehold.co/400'}
+const producto4 = { id: 4, nombre: 'Mouse', precio: 100, img: 'https://placehold.co/400'}
+const producto5 = { id: 5, nombre: 'Monitor', precio: 300, img: 'https://placehold.co/400'}
+
+const productos = [producto1, producto2, producto3, producto4, producto5]
+
+console.log(productos)
