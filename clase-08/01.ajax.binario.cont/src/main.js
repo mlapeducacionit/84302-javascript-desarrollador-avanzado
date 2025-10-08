687f5f9c60af54966fea994665879449323b3419
Maximiliano Principe <mlpeducacionit@gmail.com>
2025-10-08 15:31:29 -0300

    Destructurando objetos

--- clase-08/01.ajax.binario.cont/src/main.js
+++ clase-08/01.ajax.binario.cont/src/main.js
@@ -6,8 +6,8 @@ console.dir(formuContacto) // radiografía de la variable
 
 formuContacto.addEventListener('submit', (e) => {
     e.preventDefault() // detener el comportamiento por defecto del formulario.
-
-    const nameNombre = formuContacto[0].name
+    
+    /* const nameNombre = formuContacto[0].name
     const valueNombre = formuContacto[0].value
 
     console.log(nameNombre) // la variable
@@ -24,6 +24,50 @@ formuContacto.addEventListener('submit', (e) => {
         [nameApellido]: valueApellido
     }
 
+    console.log(dataObtenida) */
+
+    // ! Otra manera de gestionar
+
+    console.log(e.target) // nodo del etiqueta form
+
+    console.log(e.target[0].name) // name del nombre
+    console.log(e.target[0].value) // value del nombre
+    console.log(e.target[1].name) // name del apellido
+    console.log(e.target[1].value) // value del apellido
+
+    // desestructuración de objetos con alias
+    const { name: nameNombre, value: valueNombre } = e.target[0]
+    const { name: nameApellido, value: valueApellido } = e.target[1]
+
+    const dataObtenida = {
+        [nameNombre]: valueNombre,
+        [nameApellido]: valueApellido
+    }
+
     console.log(dataObtenida)
 
+    // desestructuración de objetos
+
+    const persona = {
+        nombre: 'Luis',
+        apellido: 'Gutierrez'
+    }
+
+    const persona2 = {
+        nombre: 'Nadia',
+        apellido: 'Gomez'
+    }
+
+    console.log(persona)
+    console.log(persona2)
+
+    const nombrePersona = persona.nombre
+    console.log(nombrePersona) // 'Luis'
+
+    // Desestructuración de objetos ( A partir de las keys desestructuro )
+
+    // const {} = objeto
+    const { nombre, apellido } = persona
+    console.log(nombredafdfads) // 'Luis'
+    console.log(apellido) // 'Gutierrez'
 })
