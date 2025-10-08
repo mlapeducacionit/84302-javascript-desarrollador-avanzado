bc79b6806e3a11e823c1cd7fbd93724d5fab451c
Maximiliano Principe <mlpeducacionit@gmail.com>
2025-10-08 15:56:32 -0300

    Agrego handle para trabajar con peticiones http

---
+++ clase-08/01.ajax.binario.cont/src/utils/handle-http.js
@@ -0,0 +1,33 @@
+// Función handler Http
+// INPUT: urlConcatenada y el objetos Options
+// OUTPUT: El resultado de la petición
+const handleHttp = async (urlConcatena, options = {}) => {
+
+    debugger
+
+    try {
+
+        // Fetch por defecto, hace petición GET -> 
+        const res = await fetch(urlConcatena, options)
+
+        if ( !res.ok ) {
+            throw new Error('No se pudo hacer petición')
+        }
+
+        const data = await res.json()
+        //console.log(data)
+
+        return data
+
+    } catch (error) {
+        console.error(error);
+    }
+
+
+}
+
+export default handleHttp
+
+// Modulos ESModules --> Forma moderna
+// export default <---  una funcionalidad principal (Solo un export default)
+// export <--- muchs funcionalides secundarias (Todos los que quiera, exports)
