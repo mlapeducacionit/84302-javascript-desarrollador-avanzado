4d766f16e165d60abf5fe4025f6f6c6cb09a5538
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-10-13 16:12:59 -0300

    Empezando con local y session

--- clase-10/02.persistencia-cliente/src/main.js
+++ clase-10/02.persistencia-cliente/src/main.js
@@ -1,1 +1,20 @@
 import './style.css'
+
+// LocalStorage, sessionStorage, cookies nos permiten persistir (guardar) información de nuestra aplicación en el navegador del cliente.
+
+// localStorage y sessionStorage -> Vamos a poder guardar información, clave/valor. Cadenas de caracteres, numeros, booleanos, arrays y objetos.
+// ¿Cómo se van a guardar los tipos de datos, string, number, boolean, arrays, objects? Como cadena de caracteres (strings)
+
+console.warn('Local Storage')
+
+console.log(window.localStorage)
+console.log(localStorage)
+
+console.warn('Session Storage')
+console.log(window.sessionStorage);
+console.log(sessionStorage);
+
+console.warn('// ! Obtener una variable que esta dentro del localStorage')
+console.log(window.localStorage.getItem('nombre')) // 'Maxi' 
+console.log(window.localStorage.getItem('numero')) // '22'
+console.log(window.localStorage.getItem('isVisited')) // 'true'
